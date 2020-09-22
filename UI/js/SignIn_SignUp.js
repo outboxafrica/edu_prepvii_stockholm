363c0d750388af3776bd5d21955e25151159832e
MAGOBA_DOREEN <magobadoreen4@gmail.com>
2020-09-21 17:31:27 -0700

    Added Contactus

---
+++ UI/js/SignIn_SignUp.js
@@ -0,0 +1,93 @@
+//SLIDING CAPABILITY
+const sign_in_btn = document.querySelector("#sign-in-btn");
+const sign_up_btn = document.querySelector("#sign-up-btn");
+const container = document.querySelector(".container");
+
+sign_up_btn.addEventListener("click", () => {
+    container.classList.add("sign-up-mode");
+});
+
+sign_in_btn.addEventListener("click", () => {
+    container.classList.remove("sign-up-mode");
+});
+
+
+//REGISTRATIO AND LOGIN FUNCTIONS
+var usernameArray=[];
+var emailArray=[];
+var passwordArray=[];
+
+function register(){
+    var username = document.getElementById("ru").value;
+    var email = document.getElementById("re").value;
+    var password = document.getElementById("rp").value;
+    var passwordRetype = document.getElementById("rrp").value;
+
+    if(username == ""){
+        alert("User Name required.");
+    }
+    else if (email == ""){
+        alert("Email required.");
+        return ;
+    }
+    else if (password == ""){
+        alert("Password required.");
+        return ;
+    }
+    else if (passwordRetype == ""){
+        alert("Password required.");
+        return ;
+    }
+    else if ( password != passwordRetype ){
+        alert("Password don't match retype your Password.");
+        return;
+    }
+    else if(emailArray.indexOf(email) == -1){
+        emailArray.push(email);
+        passwordArray.push(password);
+
+        alert(email + "  Thanks for registration. \nTry to login Now");
+
+        document.getElementById("re").value ="";
+        document.getElementById("rp").value="";
+        document.getElementById("rrp").value="";
+    }
+    else{
+        alert(email + " is already register.");
+        return ;
+    }
+}
+
+function login(){
+    var email = document.getElementById("se").value;
+    var password = document.getElementById("sp").value;
+
+    var i = emailArray.indexOf(email);
+
+    if(emailArray.indexOf(email) == -1){
+        if (email == ""){
+            alert("Email required.");
+            return ;
+        }
+        alert("Email does not exist.");
+        return ;
+    }
+    else if(passwordArray[i] != password){
+        if (password == ""){
+            alert("Password required.");
+            return ;
+        }
+        alert("Password does not match.");
+        return ;
+    }
+    else {
+        // alert(email + " yor are login Now \n welcome to our website.");
+
+        document.getElementById("se").value ="";
+        document.getElementById("sp").value="";
+
+        window.location="index.html";
+        return ;
+    }
+
+}
