10b4e62e6291a7a6d5bf444835be1e17ac35c436
DeroMal <derrickmal123@gmail.com>
2020-09-24 00:21:40 +0300

    return date

--- UI/js/cart.js
+++ UI/js/cart.js
@@ -11,28 +11,26 @@ function ready() {
         button.addEventListener('click', removeCartItem)
     }
 
-    var addToCartButtons = document.getElementsByClassName('shop-item-button')
+    var addToCartButtons = document.getElementsByClassName('rent-item-button')
     for (var i = 0; i < addToCartButtons.length; i++) {
         var button = addToCartButtons[i]
         button.addEventListener('click', addToCartClicked)
     }
 
-    document.getElementsByClassName('btn-purchase')[0].addEventListener('click', purchaseClicked)
+    document.getElementsByClassName('rent-BTN')[0].addEventListener('click', purchaseClicked)
 }
 
 function purchaseClicked() {
-    alert('Thank you for your purchase')
+    alert("Successfully Rented!")
     var cartItems = document.getElementsByClassName('cart-items')[0]
     while (cartItems.hasChildNodes()) {
         cartItems.removeChild(cartItems.firstChild)
     }
-    updateCartTotal()
 }
 
 function removeCartItem(event) {
     var buttonClicked = event.target
     buttonClicked.parentElement.parentElement.remove()
-    updateCartTotal()
 }
 
 
@@ -40,14 +38,12 @@ function removeCartItem(event) {
 function addToCartClicked(event) {
     var button = event.target
     var shopItem = button.parentElement.parentElement
-    var title = shopItem.getElementsByClassName('shop-item-title')[0].innerText
-    var price = shopItem.getElementsByClassName('shop-item-price')[0].innerText
-    var imageSrc = shopItem.getElementsByClassName('shop-item-image')[0].src
-    addItemToCart(title, price, imageSrc)
-    updateCartTotal()
+    var title = shopItem.getElementsByClassName('rent-item-title')[0].innerText
+    var imageSrc = shopItem.getElementsByClassName('rent-item-image')[0].src
+    addItemToCart(title, imageSrc)
 }
 
-function addItemToCart(title, price, imageSrc) {
+function addItemToCart(title, imageSrc) {
     var cartRow = document.createElement('div')
     cartRow.classList.add('cart-row')
     var cartItems = document.getElementsByClassName('cart-items')[0]
@@ -58,30 +54,28 @@ function addItemToCart(title, price, imageSrc) {
             return
         }
     }
+
+    var date = new Date()
+    d = date.getDate()
+    m = date.getMonth()
+    y = date.getFullYear()
+
+    for (i = 0; i < 3; i++) {
+        var curdate = new Date(y, m, d + i)
+        var ReturnD = curdate.toLocaleDateString();
+    }
+    dt = ReturnD
+
     var cartRowContents = `
         <div class="cart-item cart-column">
             <img class="cart-item-image" src="${imageSrc}" width="100" height="100">
             <span class="cart-item-title">${title}</span>
         </div>
-        <span class="cart-price cart-column">${price}</span>
+        <span class="cart-price cart-column">${dt}</span>
         <div class="cart-quantity cart-column">
             <button class="btn btn-danger" type="button">REMOVE</button>
         </div>` 
     cartRow.innerHTML = cartRowContents
     cartItems.append(cartRow)
     cartRow.getElementsByClassName('btn-danger')[0].addEventListener('click', removeCartItem)
-}
-
-function updateCartTotal() {
-    var cartItemContainer = document.getElementsByClassName('cart-items')[0]
-    var cartRows = cartItemContainer.getElementsByClassName('cart-row')
-    var total = 0
-    for (var i = 0; i < cartRows.length; i++) {
-        var cartRow = cartRows[i]
-        var priceElement = cartRow.getElementsByClassName('cart-price')[0]
-        var price = parseFloat(priceElement.innerText.replace('UGX', ''))
-        total = total + price
-    }
-    total = Math.round(total * 100) / 100
-    document.getElementsByClassName('cart-total-price')[0].innerText = 'UGX' + total
 }
